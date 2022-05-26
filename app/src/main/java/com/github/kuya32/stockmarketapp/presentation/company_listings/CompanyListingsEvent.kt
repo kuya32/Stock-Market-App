cf5ea4c0e4aa43a398781945fafe69999dbdd3f5
Marchael Acode <m.acode@outlook.com>
2022-05-26 13:49:12 -0700

    Created company listing viewmodel, state and events.

---
+++ app/src/main/java/com/github/kuya32/stockmarketapp/presentation/company_listings/CompanyListingsEvent.kt
@@ -0,0 +1,6 @@
+package com.github.kuya32.stockmarketapp.presentation.company_listings
+
+sealed class CompanyListingsEvent {
+    object Refresh: CompanyListingsEvent()
+    data class OnSearchQueryChange(val query: String): CompanyListingsEvent()
+}
---
+++ app/src/main/java/com/github/kuya32/stockmarketapp/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package com.github.kuya32.stockmarketapp.presentation.company_listings
+
+import com.github.kuya32.stockmarketapp.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
